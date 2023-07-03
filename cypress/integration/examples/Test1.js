a3a1024b966a61b13fa92c085e0725eabd968966
Emily Ashburn <enashburn99@gmail.com>
2023-07-03 12:15:15 -0400

    Getting a new Cypress project setup/configured. Ran first Cypress test case successfully!

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,9 @@
+// 07/03/2023 - Learning Cypress with Mocha/Chai frameworks
+
+describe('My First Test Suite', function() {
+    //All test cases for this test suite belong in here!
+    it('My First Test Case', function() {
+        //Test steps for this test case belong in here!!
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    })
+})
